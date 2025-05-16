8b065fd30dea4c23d37d7c998bd6eea8adee88a9
EugenYushin <evgenij.ushin@gmail.com>
2025-05-16 16:57:02 +0300

    Initial Commit

---
+++ decaf/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  decaf
+//
+//  Created by Eugen Yushin on 16.05.25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ decafTests/decafTests.swift
@@ -0,0 +1,17 @@
+//
+//  decafTests.swift
+//  decafTests
+//
+//  Created by Eugen Yushin on 16.05.25.
+//
+
+import Testing
+@testable import decaf
+
+struct decafTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
